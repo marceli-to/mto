e2492cbc62f26514506163bff0537d51892fe3fa
Marcel Stadelmann <m@marceli.to>
2025-12-14 16:25:31 +0100

    wip

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -10,61 +10,63 @@ use Spatie\LaravelPdf\PdfBuilder;
 
 class PdfController extends Controller
 {
-    protected string $filenamePrefix = 'mto-';
+ 	protected string $filenamePrefix = 'mto-';
 
-    public function invoice(Invoice $invoice)
-    {
-        $invoice->load(['positions', 'client']);
+ 	public function invoice(Invoice $invoice)
+ 	{
+ 		$invoice->load(['positions', 'client']);
 
-        return $this->buildPdf('pdf.invoice', ['invoice' => $invoice])
-            ->headerView('pdf.partials.header')
-            ->footerView('pdf.partials.footer')
-            ->name($this->getInvoiceFilename($invoice));
-    }
+ 		return $this->buildPdf('pdf.invoice', ['invoice' => $invoice])
+      ->headerView('pdf.partials.header')
+      ->footerView('pdf.partials.footer')
+      ->name($this->getInvoiceFilename($invoice));
+ 	}
 
-    public function expense(Expense $expense)
-    {
-        return $this->buildPdf('pdf.expense', ['expense' => $expense])
-            ->name($this->getExpenseFilename($expense));
-    }
+ 	public function expense(Expense $expense)
+ 	{
+ 		return $this->buildPdf('pdf.expense', ['expense' => $expense])
+      ->headerView('pdf.partials.header')
+      ->footerView('pdf.partials.footer')
+      ->name($this->getExpenseFilename($expense));
+ 	}
 
-    protected function buildPdf(string $view, array $data = [], array $margins = [30, 20, 30, 20]): PdfBuilder
-    {
-        $pdf = Pdf::view($view, $data)
-            ->format('a4')
-            ->margins(...$margins);
+ 	protected function buildPdf(string $view, array $data = [], array $margins = [30, 20, 30, 20]): PdfBuilder
+ 	{
+ 		$pdf = Pdf::view($view, $data)
+ 			->format('a4')
+ 			->margins(...$margins);
 
-        if (app()->environment('production')) {
-            $pdf->onLambda();
-        } else {
-            $pdf->withBrowsershot(function (\Spatie\Browsershot\Browsershot $browsershot) {
-                $browsershot
-                    ->setNodeBinary('/Users/marceli.to/.nvm/versions/node/v22.19.0/bin/node')
-                    ->setNpmBinary('/Users/marceli.to/.nvm/versions/node/v22.19.0/bin/npm');
-            });
-        }
+ 		if (app()->environment('production')) {
+ 			$pdf->onLambda();
+ 		} else {
+ 			$pdf->withBrowsershot(function (\Spatie\Browsershot\Browsershot $browsershot) {
+ 				$browsershot
+ 					->setNodeBinary('/Users/marceli.to/.nvm/versions/node/v22.19.0/bin/node')
+ 					->setNpmBinary('/Users/marceli.to/.nvm/versions/node/v22.19.0/bin/npm');
+ 			});
+ 		}
 
-        return $pdf;
-    }
+ 		return $pdf;
+ 	}
 
-    protected function getInvoiceFilename(Invoice $invoice): string
-    {
-        return sprintf(
-            '%s%s-%s-%s.pdf',
-            $this->filenamePrefix,
-            $invoice->number,
-            $invoice->client->acronym,
-            Str::slug(str_replace('www.', '', $invoice->title))
-        );
-    }
+ 	protected function getInvoiceFilename(Invoice $invoice): string
+ 	{
+ 		return sprintf(
+ 			'%s%s-%s-%s.pdf',
+ 			$this->filenamePrefix,
+ 			$invoice->number,
+ 			$invoice->client->acronym,
+ 			Str::slug(str_replace('www.', '', $invoice->title))
+ 		);
+ 	}
 
-    protected function getExpenseFilename(Expense $expense): string
-    {
-        return sprintf(
-            'mto-%s-%s-%s.pdf',
-            $expense->date->format('d.m.Y'),
-            Str::slug($expense->title),
-            $expense->number
-        );
-    }
+ 	protected function getExpenseFilename(Expense $expense): string
+ 	{
+ 		return sprintf(
+ 			'mto-%s-%s-%s.pdf',
+ 			\Carbon\Carbon::parse($expense->date)->format('d.m.Y'),
+ 			Str::slug($expense->title),
+ 			$expense->number
+ 		);
+ 	}
 }
---
+++ resources/views/pdf/expense.blade.php
@@ -0,0 +1,146 @@
+@php
+  $fontPath = resource_path('sidecar-browsershot/fonts/');
+  $fontLight = base64_encode(file_get_contents($fontPath . 'Poppins-Light.woff2'));
+  $fontMedium = base64_encode(file_get_contents($fontPath . 'Poppins-Medium.woff2'));
+@endphp
+<html lang="en">
+<head>
+<title>Ausgabe</title>
+<script src="https://cdn.tailwindcss.com"></script>
+<style>
+  @font-face {
+    font-family: 'Poppins';
+    src: url('data:font/woff2;base64,{{ $fontLight }}') format('woff2');
+    font-weight: 300;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Poppins';
+    src: url('data:font/woff2;base64,{{ $fontMedium }}') format('woff2');
+    font-weight: 500;
+    font-style: normal;
+  }
+  strong {
+    font-weight: 500;
+  }
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
+  
+  th {
+    border-bottom: .1mm solid #000000;
+    text-align: left;
+    font-weight: 500;
+    padding-bottom: 2mm;
+  }
+
+  td {
+    border-bottom: .1mm solid #000000;
+    font-weight: 300;
+    vertical-align: top;
+    padding-bottom: 2mm;
+    padding-top: 2mm;
+  }
+
+  tr {
+    break-inside: avoid;
+  }
+
+  .font-size-xs {
+    font-size: 9pt;
+    line-height: 13pt;
+  }
+  
+  .font-size-sm {
+    font-size: 10pt;
+    line-height: 15pt;
+  }
+
+  .font-size-md {
+    font-size: 12pt;
+    line-height: 16pt;
+  }
+
+  .font-size-lg {
+    font-size: 16pt;
+    line-height: 20pt;
+  }
+
+  .text-highlight {
+    color: #e94364;
+  }
+
+  .expense-header {
+    margin-bottom: 10mm;
+    margin-top: 10mm;
+  }
+
+  .expense-positions {
+    margin-top: 5mm;
+    border-top: .1mm solid #000000;
+  }
+
+  .page-break {
+    page-break-after: always;
+  }
+
+  .receipt-container {
+    margin-top: 35mm;
+    max-height: 220mm;
+    max-width: 160mm;
+    overflow: hidden;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .receipt-container img {
+    width: auto;
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+</style>
+</head>
+<body class="font-light">
+
+<div>
+
+  <!-- Expense Header -->
+  <div class="expense-header">
+    <h1 class="font-size-lg">
+      <strong class="text-highlight">Ausgabe</strong>
+    </h1>
+  </div>
+
+  <!-- Expense Title -->
+  <div class="font-size-md">
+    <strong>{{ $expense->title }}</strong>
+  </div>
+
+  <!-- Expense Details -->
+  <table class="expense-positions font-size-sm w-full">
+    <tbody>
+      <tr>
+        <td>{{ $expense->description }}, {{ \Carbon\Carbon::parse($expense->date)->format('d.m.Y') }}</td>
+        <td style="text-align: right; width: 25%;">{{ number_format($expense->amount, 2, '.', "'") }}</td>
+      </tr>
+      <tr>
+        <td style="border-bottom: 0.6mm solid #000000;"><strong>Total {{ $expense->currency }}</strong></td>
+        <td style="text-align: right; border-bottom: 0.6mm solid #000000;"><strong>{{ number_format($expense->amount, 2, '.', "'") }}</strong></td>
+      </tr>
+    </tbody>
+  </table>
+
+</div>
+
+<!-- Page Break -->
+<div class="page-break"></div>
+
+<!-- Receipt Image -->
+<div class="receipt-container">
+  <img src="{{ public_path('storage/media/expenses/' . $expense->number . '.jpg') }}" alt="Receipt">
+</div>
+
+</body>
+</html>
