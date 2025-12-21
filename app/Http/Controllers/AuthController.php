88d2fc7cece3a65d0d2a29c8dff279de0258084c
Marcel Stadelmann <m@marceli.to>
2025-12-21 20:54:44 +0100

    adds dashboard

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,7 +38,7 @@ class AuthController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended('/admin');
+        return redirect()->intended('/dashboard');
     }
 
     /**
